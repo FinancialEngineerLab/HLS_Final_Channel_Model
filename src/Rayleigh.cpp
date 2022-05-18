7d9b98e2ee0dbe59b9430989f23253305446789a
H2SO4 <caota985107@gmail.com>
2022-05-18 19:20:01 +0800

    v9

--- src/Rayleigh.cpp
+++ src/Rayleigh.cpp
@@ -5,8 +5,8 @@
 
 void normal_rng(const int num,
                     ap_uint<32> SEED,
-                    double result_randn_Re[SAMPLE_NUM],
-                    double result_randn_Im[SAMPLE_NUM]
+                    double output_randn_Re[SAMPLE_NUM],
+                    double output_randn_Im[SAMPLE_NUM]
                     );
 
 void Rayleigh(const int num,
@@ -18,17 +18,20 @@ void Rayleigh(const int num,
     
 
     //宣告
-    double result_randn_Re[sampleNum];
-    double result_randn_Im[sampleNum];
-    double Hr[sampleNum];
-    double Hi[sampleNum];
 
-    ap_uint<32> SEED;
+
+    double result_randn_Re[SAMPLE_NUM];
+    double result_randn_Im[SAMPLE_NUM];
+    double Hr[SAMPLE_NUM];
+    double Hi[SAMPLE_NUM];
+
+    //ap_uint<32> SEED;
 
     //呼叫normal_rng，產生一組32個normal distribution values
-    normal_rng(sampleNum, SEED, result_randn_Re, result_randn_Im);
+    normal_rng(SAMPLE_NUM, SEED, result_randn_Re, result_randn_Im);
 
-    double Hr[16], Hi[16]; //參數化??
+    double Hr[16];
+    double Hi[16]; //參數化??
 
     for(int j=0; j<16; j++){
         Hr[j] = result_randn_Re[j] / sqrt(2);
--- src/normal_rng.hpp
+++ src/normal_rng.hpp
@@ -1,5 +1,5 @@
 #ifndef __DUT_HPP_
 
-#define SAMPLE_NUM (1 << 10)
+#define SAMPLE_NUM (1 << 4)
 
 #endif
