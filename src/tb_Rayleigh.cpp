6663e1d757663e21b21ccb2a1811de55f218281f
H2SO4 <caota985107@gmail.com>
2022-05-18 19:44:19 +0800

    v17

--- src/tb_Rayleigh.cpp
+++ src/tb_Rayleigh.cpp
@@ -39,7 +39,7 @@ int main() {
     std::cout<<" output_randn["<< i <<"]: "<<Ray_Hi[i]<<std::endl;
     }
 
-    std::cout<< 1/sqrt(2) <<std::endl;
+    std::cout<< "\n sqrt(1/2)= " << 1/sqrt(2) <<std::endl;
 
 	return 0;
         // std::cout << i << " : " << resultMT19937BoxMuller[i] << " ,sum: " << avgMT19937BoxMuller << std::endl;
