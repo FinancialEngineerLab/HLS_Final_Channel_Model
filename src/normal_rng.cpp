696dd1ca22e26b3b06f1b4dd60c7cfadc08d1079
H2SO4 <caota985107@gmail.com>
2022-05-18 17:16:31 +0800

    double v2 可以跑 16個sample

--- src/normal_rng.cpp
+++ src/normal_rng.cpp
@@ -18,6 +18,9 @@ extern "C" void normal_rng(const int num,
 	xf::fintech::MT19937IcnRng<double> rngMT19937ICN;
 	rngMT19937ICN.seedInitialization(SEED);
 
+	double output_randn_Re[SAMPLE_NUM];
+	double output_randn_Im[SAMPLE_NUM];
+
 	for (int i=0; i<num; i++){
 	#pragma HLS pipeline II=1
 		output_randn_Re[i] = rngMT19937ICN.next();
