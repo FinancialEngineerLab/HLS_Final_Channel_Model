59609913d55b838c08994299218fa338c99cd6fa
H2SO4 <caota985107@gmail.com>
2022-05-19 19:29:40 +0800

    v24 新增set top function

---
+++ code/tests/Modulation.cpp
@@ -0,0 +1,41 @@
+#include <ap_int.h>
+#include "xf_fintech/rng.hpp"
+#include <math.h>
+#include <iostream>
+#include "normal_rng.hpp"
+
+
+extern "C" void Rayleigh(const int num,
+ 			   ap_uint<32> SEED,
+			   double Hr[SAMPLE_NUM],
+			   double Hi[SAMPLE_NUM]
+			   ){
+
+    
+	xf::fintech::MT19937IcnRng<double> rngMT19937ICN;
+	rngMT19937ICN.seedInitialization(SEED);
+
+    //宣告
+
+
+    //double result_randn_Re[SAMPLE_NUM];
+    //double result_randn_Im[SAMPLE_NUM];
+    //double Hr[SAMPLE_NUM];
+    //double Hi[SAMPLE_NUM];
+
+    //ap_uint<32> SEED;
+
+    //呼叫normal_rng，產生一組32個normal distribution values
+    //normal_rng(SAMPLE_NUM, SEED, result_randn_Re, result_randn_Im);
+
+    //double Hr[16];
+    //double Hi[16]; //參數化??
+
+    for(int j=0; j<num; j++){
+        Hr[j] = rngMT19937ICN.next() / sqrt(2);
+        Hi[j] = rngMT19937ICN.next() / sqrt(2);
+    }
+
+
+
+}
--- code/tests/run_hls.tcl
+++ code/tests/run_hls.tcl
@@ -39,7 +39,7 @@ open_project -reset $PROJ
 ########## Test AWGN
 add_files "Rayleigh.cpp AWGN.cpp" -cflags "-I${XF_PROJ_ROOT}/code/include -I${XF_PROJ_ROOT}/ext/dcmt/dcmt/include"
 add_files -tb "tb_AWGN.cpp ${XF_PROJ_ROOT}/ext/dcmt/dcmt/lib/libdcmt.a" -cflags "-I${XF_PROJ_ROOT}/code/include -I${XF_PROJ_ROOT}/ext/dcmt/dcmt/include"
-set_top dut
+set_top Rayleigh
 
 
 open_solution -reset $SOLN
